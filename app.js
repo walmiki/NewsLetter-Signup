6ed5fd37920f501da0676bd7084f58d11acc61a7
walmiki <walmiki240@gmail.com>
2020-05-29 21:39:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  var firstName = req.body.n1;
+  var lastName = req.body.n2;
+  var email = req.body.e1;
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  var jsonData = JSON.stringify(data);
+  url = "https://us18.api.mailchimp.com/3.0/lists/2cb0d6a751";
+  options = {
+    method: "POST",
+    auth: "walmiki240:eeee661fdfde6938565c817775a38653-us18",
+  };
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+  });
+  request.write(jsonData);
+  request.end();
+});
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("server is running on port 3000");
+});
+
+// api key
+// eeee661fdfde6938565c817775a38653-us18
+// list Id
+// 2cb0d6a751
